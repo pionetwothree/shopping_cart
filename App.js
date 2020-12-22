e04b307710cd8959a2379cf9bf7bb1c114a0a0bb
pionetwothree <75122491+pionetwothree@users.noreply.github.com>
2020-12-22 16:06:28 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import TabScreen from './src/screens/TabScreen';
+//import {Platform, Stylesheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return <TabScreen />;
+  }
+}
